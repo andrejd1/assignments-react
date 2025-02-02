5680e2aa47d2cd17228334b35b9b9ad501313153
Thien Long Ngo <ngo.andrej@gmail.com>
2025-02-02 23:40:13 +0100

    chore(client): F7 - sort the list

--- client/src/hooks/useTodoItems.ts
+++ client/src/hooks/useTodoItems.ts
@@ -4,7 +4,13 @@ import { ListItemType } from "../components/todo/ListItem/ListItemType";
 const fetchTodoItems = async (): Promise<ListItemType[]> => {
     const response = await fetch("http://localhost:3000/items");
     const items: ListItemType[] = await response.json();
-    return items.reverse();
+
+    return items.sort((a, b) => {
+        if (a.isDone === b.isDone) {
+            return b.createdAt - a.createdAt;
+        }
+        return a.isDone ? 1 : -1;
+    });
 };
 
 export const useTodoItems = () => {
