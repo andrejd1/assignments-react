c341828f2a43f810ba74659784d848a4e08c877d
Thien Long Ngo <ngo.andrej@gmail.com>
2025-02-03 00:05:03 +0100

    chore(client): U3 - ListItem actions visibility

--- client/src/components/todo/ListItem/StyledListItem.ts
+++ client/src/components/todo/ListItem/StyledListItem.ts
@@ -1,20 +1,25 @@
 import styled from "styled-components";
 
+export const StyledListButtonContainer = styled.div`
+    display: flex;
+    visibility: hidden;
+    gap: 0.5rem;
+`;
+
 export const StyledListItem = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
     padding: 0.25rem 0;
     width: 100%;
+
+    &:hover ${StyledListButtonContainer} {
+        visibility: visible;
+    }
 `;
 
 export const StyledListContainer = styled.div`
     display: flex;
-`;
-
-export const StyledListButtonContainer = styled.div`
-    display: flex;
-    gap: 0.5rem;
 `;
 
 export const StyledListItemLabel = styled.label`
